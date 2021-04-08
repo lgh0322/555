d0684ea3de33678ccc0b32f5b5a9e12db17ff8ce
李光浩 <liguanghao@viatomtech.com>
2021-04-08 21:50:42 +0800

    +6

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http');
+var bodyParser = require('body-parser');
 const server = http.createServer(app);
 
 app.get('/sdj', (req, res) => {
@@ -9,6 +10,17 @@ app.get('/sdj', (req, res) => {
     console.log(req.query)
     res.sendFile(__dirname + '/gu.FLAC')
 });
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.post('/two', urlencodedParser, function (req, res) {
+ 
+  var response = {
+      "first_name":req.body.fuck,
+      "last_name":req.body.fuck2
+  };
+  console.log(response);
+  res.send(JSON.stringify(response));
+ })
 
 server.listen(8080, () => {
   console.log('listening on *:8080');
